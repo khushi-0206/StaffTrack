54329672be20be25edbaf5fa20fe17598b5c63cc
Khushi <khushimehta0206@gmail.com>
2026-04-07 13:20:09 +0530

    Added APIGateway project

--- APIGateway/obj/Debug/net8.0/APIGateway.API.AssemblyInfo.cs
+++ APIGateway/obj/Debug/net8.0/APIGateway.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIGateway.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a570f8947111e1caba9baa55d2a34c1818c5534")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa21a766b347621ba412a12cdb0b6db00286c917")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIGateway.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIGateway.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
